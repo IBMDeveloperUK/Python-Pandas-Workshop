c1eeee47b04f34c3ca7db6902a7f57a85c7c08be
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2019-02-19 10:03:26 +0000

    Create answers4.py

---
+++ answers/answers4.py
@@ -0,0 +1,3 @@
+avgHeightFront = (jeans.maxHeightFront + jeans.minHeightFront) / 2
+avgWidthFront = (jeans.maxWidthFront + jeans.minWidthFront) / 2
+jeans['FrontArea'] = avgHeightFront * avgWidthFront
