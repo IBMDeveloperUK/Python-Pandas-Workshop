978b17f7aea1056edb0d77bdc9e07678047c78b9
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2019-07-30 19:48:17 +0100

    Update answer9.py

--- answers/answer9.py
+++ answers/answer9.py
@@ -1,6 +1,6 @@
-ax = jeans['BackArea'][jeans['menWomen']=='men'].plot.hist(
+ax = jeans['FrontArea'][jeans['menWomen']=='men'].plot.hist(
     bins=15,figsize=(10,5),alpha=0.5);
-ax = jeans['BackArea'][jeans['menWomen']=='women'].plot.hist(
+ax = jeans['FrontArea'][jeans['menWomen']=='women'].plot.hist(
     bins=15,figsize=(10,5),alpha=0.5);
-ax.set_ylabel('Back Pocket Size');
+ax.set_ylabel('Front Pocket Size');
 ax.legend(['men','women']);
