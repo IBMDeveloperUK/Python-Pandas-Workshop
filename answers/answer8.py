fd1efc196ffdb820f7d015ccf53afbde32be2355
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2020-03-04 13:14:21 +0000

    Delete answer8.py

--- answers/answer8.py
+++
@@ -1,9 +0,0 @@
-menfront=(jeans['FrontArea'][jeans['men'] == 1].mean())
-menback=(jeans['BackArea'][jeans['men'] == 1].mean())
-womenfront=(jeans['FrontArea'][jeans['women'] == 1].mean())
-womenback=(jeans['BackArea'][jeans['women'] == 1].mean())
-
-print('Avg men front pocket size is ' + str(menfront)) 
-print('Avg men back pocket size is ' + str(menback)) 
-print('Avg women front pocket size is ' + str(womenfront)) 
-print('Avg women back pocket size is ' + str(womenback)) 
