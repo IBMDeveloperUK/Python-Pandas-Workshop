65c3a200227638b5caa1a20809f3e17140040205
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2020-03-04 13:01:03 +0000

    Update answer2.py

--- answers/answer2.py
+++ answers/answer2.py
@@ -1,1 +1,5 @@
-jeans[jeans['price'] == jeans['price'].min()]
+print (boroughs['New migrant rates'].min())
+print (boroughs['Name'][boroughs['New migrant rates'] == boroughs['New migrant rates'].min()])
+
+print (boroughs['New migrant rates'].max())
+print (boroughs['Name'][boroughs['New migrant rates'] == boroughs['New migrant rates'].max()])
