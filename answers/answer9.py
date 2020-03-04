99ed504435676b0dd8614507ea410d98811b31b1
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2020-03-04 13:14:30 +0000

    Delete answer9.py

--- answers/answer9.py
+++
@@ -1,6 +0,0 @@
-ax = jeans['FrontArea'][jeans['menWomen']=='men'].plot.hist(
-    bins=15,figsize=(10,5),alpha=0.5);
-ax = jeans['FrontArea'][jeans['menWomen']=='women'].plot.hist(
-    bins=15,figsize=(10,5),alpha=0.5);
-ax.set_ylabel('Front Pocket Size');
-ax.legend(['men','women']);
